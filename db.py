8fe881283ac51f2690eacad9aa237d18afc39bdf
Samet Kurt <43661157+skurt1996@users.noreply.github.com>
2023-12-21 19:25:54 +0100

    Update db.py

--- db.py
+++ db.py
@@ -9,10 +9,6 @@ import sql_statements as sql
 import data_retrieval as api
 from utils import iso8601_to_datetime
 
-# Dictionary is needed to use the table_exists method
-TABLE_CREATION_QUERIES = {"PLAYERS" : sql.PLAYERS,
-                          "BATTLES" : sql.BATTLES,
-                          "SCORES" : sql.SCORES}
 ALLOWED_BATTLE_TYPE = "clanMate"
 ALLOWED_BATTLE_MODES = ["PickMode", "DraftMode", "Draft_Competitive", "ClassicDecks_Friendly", "Duel_1v1_Friendly"]
 
@@ -21,20 +17,6 @@ logger = configured_logger("db.log")
 def create_connection(host="localhost", port=3306, user=os.environ.get("MYSQL_USERNAME"), password=os.environ.get("MYSQL_PASSWORD"), database="clash_royale"):
     """
     Creates a connection to the clash_royale database.
-
-    Args:
-        host (str): The hostname or IP address of the MySQL server.
-        port (int): The port number to connect to the MySQL server.
-        user (str): The username for authenticating with the MySQL server.
-        password (str): The password for authenticating with the MySQL server.
-        database (str): The name of the database to connect to.
-
-    Returns:
-        cnx: A connection object representing the connection to the clash_royale database.
-            The connection object can be used to execute database operations.
-
-    Raises:
-        mysql.connector.Error: If an error occurs while establishing the connection.
     """
     cnx = None
     try:
@@ -55,13 +37,6 @@ def table_exists(cursor, table_name):
     """
     Checks if a table exists in the database. 
     Used in initialize_tables()
-
-    Args:
-        cursor: Database cursor object.
-        table_name (str): Name of the table.
-
-    Returns:
-        bool: True if the table exists, False otherwise.
     """
     cursor.execute("SHOW TABLES")
     tables = cursor.fetchall()
@@ -72,11 +47,6 @@ def create_table(cursor, table_name, query):
     """
     Creates a table in the database.
     Used in initialize_tables()
-
-    Args:
-        cursor: Database cursor object.
-        table_name (str): Name of the table.
-        query (str): SQL query to create the table.
     """
     cursor.execute(query)
     logger.info(f"Created table: {table_name}")
@@ -86,6 +56,9 @@ def initialize_tables():
     """
     Connects to the clash_royale database and creates tables if they do not exist.
     """
+    TABLE_CREATION_QUERIES = {"PLAYERS" : sql.PLAYERS, 
+                              "BATTLES" : sql.BATTLES,
+                              "SCORES" : sql.SCORES}
     try:
         with create_connection() as cnx:
             with cnx.cursor() as cursor:
@@ -103,19 +76,6 @@ def insert_with_error_handling(cnx, query, params, recursive_insertion=False):
     Executes an insert query with parameters and handles errors. If a duplicate entry error occurs and
     `recursive_insertion` is True, the function will retry the insertion with updated parameters by
     recursively calling itself with the remaining parameters.
-
-    Args:
-        cnx (mysql.connector.MySQLConnection): The MySQL connection object.
-        query (str): The SQL insert query.
-        params (list or tuple): The parameter values to be inserted into the query.
-        recursive_insertion (bool, optional): Specifies whether recursive insertion should be attempted
-            in case of a duplicate entry error. Defaults to False.
-
-    Returns:
-        bool: Returns True if the query was successfully executed, False otherwise.
-
-    Raises:
-        mysql.connector.Error: If an error occurs during query execution.
     """
     try:
         with cnx.cursor() as cursor:
@@ -141,11 +101,6 @@ def select_with_error_handling(cnx, query, params):
     """
     Executes a select query with parameters and error handling. 
     If params is None or empty, no sql statements are executed.
-
-    Input:
-        cnx: MySQLConnection
-        query: str
-        params: parameter tuples
     """
     try:
         with cnx.cursor() as cursor:
@@ -166,12 +121,6 @@ def select_with_error_handling(cnx, query, params):
 def get_player_id_by_name(player_name):
     """
     Retrieves the player ID for a given player_name from the "players" table.
-
-    Args:
-        player_name (str): Name of the player.
-
-    Returns:
-        int or None: The player ID if found, None otherwise.
     """
     with create_connection() as cnx:
         query = "SELECT id FROM players WHERE name = %s"
@@ -184,12 +133,6 @@ def get_player_id_by_name(player_name):
 def get_player_name_by_id(id):
     """
     Retrieves the player name for a given player id from the "players" table.
-
-    Args:
-        player_name (str): Name of the player.
-
-    Returns:
-        int or None: The player ID if found, None otherwise.
     """
     with create_connection() as cnx:
         query = "SELECT name FROM players WHERE id = %s"
@@ -202,12 +145,6 @@ def get_player_name_by_id(id):
 def get_player_name_by_tag(player_tag):
     """
     Retrieves the player name for a given tag from the "players" table.
-
-    Args:
-        player_name (str): Name of the player.
-
-    Returns:
-        int or None: The player ID if found, None otherwise.
     """
     with create_connection() as cnx:
         query = "SELECT name FROM players WHERE tag = %s"
@@ -220,12 +157,6 @@ def get_player_name_by_tag(player_tag):
 def get_player_info(player_tag):
     """
     Retrieves all column informations for a given player tag from the "players" table.
-
-    Args:
-        player_tag (str): Tag of the player.
-
-    Returns:
-        Dictionary of column informations or None if player not found.
     """
     with create_connection() as cnx:
         query = "SELECT * FROM players WHERE tag = %s"
@@ -240,9 +171,6 @@ def get_player_info(player_tag):
 def get_player_tags():
     """
     Retrieves all tags from the "players" table.
-
-    Returns:
-        list: A list of tags.
     """
     with create_connection() as cnx:
         query = "SELECT tag FROM players"
@@ -255,9 +183,6 @@ def get_player_tags():
 def get_players():
     """
     Retrieves all tags, names etc. from the "players" table.
-
-    Returns:
-        list: A list of tuples with tags, names,...
     """
     with create_connection() as cnx:
         query = "SELECT tag, name, 1v1_battle_count, 1v1_win_count, 1v1_three_crowns_win_count FROM players"
@@ -273,12 +198,6 @@ def get_players():
 def extract_battles(battles):
     """
     Processes the output of get_battles. Returns a list of dictionaries, containing battle data.
-
-    Args:
-        battles (List of tuples): Tuples contain battle data.
-
-    Returns:
-        list: A list of dictionaries of battle data.
     """
     player_ids = set()
 
@@ -315,16 +234,6 @@ def extract_battles(battles):
 def get_query_and_params(latest_battle_time, game_mode, player_tag, enemy_tag, limit):
     """
     Generate a tuple of SQL query and its parameters based on input arguments.
-
-    Parameters:
-        latest_battle_time (str): The timestamp for filtering battles.
-        game_mode (str): The game mode for filtering battles.
-        player_tag (str): The player"s tag for filtering battles.
-        enemy_tag (str): The enemy player"s tag for filtering battles.
-        limit (bool): A flag indicating whether to limit the number of results.
-
-    Returns:
-        tuple: A tuple containing the SQL query and its parameters for database querying.
     """
     query_params_dict = {
         # All battles
@@ -365,23 +274,6 @@ def get_battles(latest_battle_time="2200-12-31", game_mode="ALL", player_tag=Non
     
     """
     Retrieves a list of battle information dictionaries for the given parameters.
-
-    Args:
-        player_tag (str): The tag of the player to retrieve battles for.
-        latest_battle_time (str): Needed to query the battles before this battle_time (see SQL statement)
-        game_mode (str): The game mode to filter battles by. Default is "ALL".
-        enemy_tag (str): The tag of the enemy player. Default is None. That means all enemies are considered.
-        limit (bool): If True, only 10 elements are retrieved.
-    
-    Returns:
-        list: A list of dictionaries of battle data.
-
-    Note:
-        Returns empty list if player_tag not in the database or if no battles are given for the player_tag.
-
-    Raises:
-        TypeError: If there"s an issue with the input types or data.
-        mysql.connector.Error: If there"s an error during the database query.
     """
     with create_connection() as cnx:
         try:
@@ -403,13 +295,6 @@ def get_battles(latest_battle_time="2200-12-31", game_mode="ALL", player_tag=Non
 def parse_princess_tower_hp(princess_tower_hp):
     """
     Parses the princess tower HP data and extracts the individual tower HP values.
-
-    Args:
-        princess_tower_hp (str): Princess tower HP data as a string representation of a list.
-
-    Returns:
-        tuple: A tuple containing the princess tower HP values. If the input is None, returns (0, 0).
-               If there is only one tower HP value, returns (tower_hp, 0). Otherwise, returns (tower_hp1, tower_hp2).
     """
     princess_tower_hp = ast.literal_eval(princess_tower_hp)
     if princess_tower_hp is None:
@@ -432,17 +317,6 @@ def insert_members():
 def insert_battle(cursor, battle_info):
     """
     Inserts a battle record into the "battles" table.
-
-    Args:
-        cursor: MySQLCursor
-        battle_info: tuple
-            Information about the battle, including battle time, type, and game mode.
-
-    Returns:
-        bool: True if the insertion was successful, False otherwise.
-
-    Raises:
-        mysql.connector.Error: If an error occurs during query execution.
     """
     query = "INSERT INTO battles (time, type, game_mode) VALUES (%s, %s, %s)"
     try:
@@ -455,28 +329,6 @@ def insert_battle(cursor, battle_info):
 def insert_score(cursor, player_data, player_deck, battle_id):
     """
     Inserts a score record into the "scores" table.
-
-    Args:
-        cursor: MySQLCursor
-        crowns: int
-            Number of crowns achieved in the battle.
-        king_tower_hp: int
-            Hit points of the player"s King Tower.
-        princess_tower_1_hp: int
-            Hit points of the player"s first Princess Tower.
-        princess_tower_2_hp: int
-            Hit points of the player"s second Princess Tower.
-        elixir_leaked: int
-            Amount of elixir leaked by the player.
-        deck_string: str
-            String representation of the player"s deck.
-        battle_id: int
-            ID of the battle the score belongs to.
-        player_id: int
-            ID of the player.
-
-    Raises:
-        mysql.connector.Error: If an error occurs during query execution.
     """
     query = """
     INSERT INTO scores (crowns, king_tower_hp, princess_tower_1_hp, princess_tower_2_hp,
@@ -502,15 +354,6 @@ def check_for_battle_duplicate(time, player1_id, player2_id):
     
     This method is necessary due to a bug in the clash royale system:
     Sometimes the time of battles can differ by 1 second in the battle logs of different players.
-
-    Parameters:
-        time (str): The time in the format "%Y-%m-%d %H:%M:%S".
-        player1_id (int): The ID of the first player.
-        player2_id (int): The ID of the second player.
-
-    Returns:
-        bool: True if a duplicate battle with the same time and players" IDs exists,
-              False otherwise.
     """
     # Prepare datetime variables
     datetime_format = "%Y-%m-%d %H:%M:%S"
@@ -538,9 +381,6 @@ def insert_new_battles():
 
     Retrieves player tags, fetches battles for each player, and inserts the battles and scores into the 
     corresponding table.
-
-    Raises:
-        mysql.connector.Error: If an error occurs during database operations.
     """
     player_tags = get_player_tags()
     for player_tag in player_tags:
@@ -584,9 +424,6 @@ def update_player_infos():
     Updates the columns "1v1_battle_count", "1v1_win_count" and "1v1_three_crowns_win_count" 
     for all players in the "players" table by calculating them from the retrieved data
     battles data from the database.
-
-    Raises:
-        mysql.connector.Error: If an error occurs during the query execution.
     """
     player_tags = get_player_tags()
     with create_connection() as cnx:
@@ -629,6 +466,9 @@ def update_player_infos():
             logger.warning(TX_ROLLBACK_MSG)
     
 def stats_versus(player_tag1, player_tag2, game_mode="ALL"):
+    """
+    Calculate and return statistics for battles between two players in a specified game mode.
+    """
     battles = get_battles(game_mode=game_mode, player_tag=player_tag1, enemy_tag=player_tag2, limit=False)
     
     battle_count = len(battles)
