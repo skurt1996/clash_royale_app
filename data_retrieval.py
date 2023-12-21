16f550ade190608c0cb97ab1b1cd0ab3bff609f6
Samet Kurt <43661157+skurt1996@users.noreply.github.com>
2023-12-21 19:34:26 +0100

    Update data_retrieval.py

--- data_retrieval.py
+++ data_retrieval.py
@@ -19,12 +19,6 @@ PLAYER_TAG = "#2J200GLG8" # Spieler: Raue Hände
 def fetch_clan_members(clan_tag=CLAN_TAG):
     """
     Fetches and returns the members of a clan from the API.
-
-    Args:
-        clan_tag (str): The clan"s tag. Defaults to CLAN_TAG.
-
-    Returns:
-        list: A list of tuples representing the clan members. Each tuple contains the member"s tag, name.
     """
     END_POINT = "/clans/%23" + clan_tag[1:] + "/members"
     clan_members = []
@@ -39,19 +33,7 @@ def fetch_clan_members(clan_tag=CLAN_TAG):
 
 def fetch_battle_log(player_tag=PLAYER_TAG):
     """
-    Fetches and return the battle log of a player.
-
-    Args:
-        player_tag (str): The player"s tag. Defaults to PLAYER_TAG.
-
-    Returns:
-        list: A list of battles, each represented as a tuple containing the following information:
-            - battle_info (tuple): Information about the battle, including battle time, type, and game mode.
-            - team_info (list): Information about the player"s team, including name, crowns, elixir leaked, king tower hit points, and princess towers hit points.
-            - player_info (list): Information about the player, including name, crowns, elixir leaked, king tower hit points, and princess towers hit points.
-            - opponent_info (list): Information about the opponent, including name, crowns, elixir leaked, king tower hit points, and princess towers hit points.
-            - player_deck (list): The player"s deck, represented as a list of card names.
-            - opponent_deck (list): The opponent"s deck, represented as a list of card names.
+    Fetches and returns the battle log of a player.
     """
     END_POINT = "/players/%23" + player_tag[1:] + "/battlelog"
     try:
