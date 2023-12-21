58484dd29777fb62468f1a17b7c02791ee42c881
Samet Kurt <43661157+skurt1996@users.noreply.github.com>
2023-12-21 20:14:02 +0100

    Update logger.py

--- logger.py
+++ logger.py
@@ -1,24 +1,11 @@
 """
-logger used by db.py
+Logger used by db.py
 """
 import logging
 
 def configured_logger(file_name):
     """
     Configures and returns a logger object for logging database operations.
-
-    Returns:
-        logging.Logger: The configured logger object.
-
-    The logger is configured with the following settings:
-    - Log level: DEBUG
-    - File handler: Logs are written to a file named 'db.log' with DEBUG level.
-    - Console handler: Logs are printed to the console with DEBUG level.
-
-    Usage:
-        logger = configure_logger()
-        logger.debug('This is a debug message')
-        logger.info('This is an info message')
     """
     logger = logging.getLogger(__name__)
     logger.setLevel(logging.DEBUG)
